90ac80c1cf790679e30e2d59cf437dc5ddf5a9da
Larry Yu <laryu@cisco.com>
2018-07-19 13:21:59 -0700

    Removed AuthenticationGuard from home

--- abiegn-client/src/app/core/route.service.ts
+++ abiegn-client/src/app/core/route.service.ts
@@ -18,7 +18,7 @@ export class Route {
       path: '',
       component: ShellComponent,
       children: routes,
-      canActivate: [AuthenticationGuard],
+      canActivate: [],
       // Reuse ShellComponent instance when navigating between child views
       data: { reuse: true }
     };
