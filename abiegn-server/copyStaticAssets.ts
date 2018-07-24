8f56c0127d64709ed5c6f1833a826a232c8ff5b9
Larry Yu <laryu@cisco.com>
2018-07-24 16:45:14 -0700

    made shelljs a dev dependency instead of runtime

--- abiegn-server/copyStaticAssets.ts
+++ abiegn-server/copyStaticAssets.ts
@@ -2,3 +2,6 @@ import * as shell from "shelljs";
 
 // Use default ssl key and certificate unless overriden by production settings
 shell.cp("-R", "ssl", "dist/ssl");
+
+// Copy environment variables
+shell.cp("environments/env.prd", "dist/.env");
